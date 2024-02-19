8faf4781745f533d5c1683ba1e2cf748e9d25e8c
maxtm13 <maxtm1@yandex.ru>
2024-02-19 23:24:56 +0300

    html complete

--- sender.php
+++ sender.php
@@ -1,2 +1,38 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300..800&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+
 <?
-print_r($_POST);
+    $email = htmlspecialchars($_POST['email']);
+    $phone = htmlspecialchars($_POST['phone']);
+    //echo('Ваш email: ' . $email . ', Ваш телефон: ' . $phone);
+
+    //
+    $to = 'maxtm1@yandex.ru';
+    $subject = 'заявка ' . 'Тихонов';
+    $message = 'Email из формы: ' . $email . "\n" . ' Телефон: ' . $phone;
+    $headers = 'From: webmaster@forma-test' . "\r\n" .
+        'Reply-To: webmaster@forma-test' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+    ?>
+    <div class="content sender">
+        <?
+        if (mail($to, $subject, $message, $headers)) {?>
+            <h2 class="title success">Ваше сообщение отправленно!</h2>
+        <? }
+        else {  ?>
+            <h2 class="title warning">Сообщение не отправленно, попробуйте позже</h2>
+        <? } ?>
+        <a href="/" class="back">ОК</a>
+    </div>
+</body>
+</html>
