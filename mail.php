d18fe566ef9e5bf1d3d704f43992926702c0d980
Tikhonov Maxim <121362225+maxtm13@users.noreply.github.com>
2024-02-21 20:28:41 +0300

    Delete mail.php

--- mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = 'maxtm1@yandex.ru';
-$subject = 'заявка ' . 'Тихонов';
-$message = 'Email из формы: ' . '$email' . "\r\n" . 'Телефон: ' . '$phone' . "\r\n";
-$headers = 'Content-Type: text/html; charset=utf-8' . "\r\n" . 'From: webmaster@forma-test' . "\r\n" .
-    'Reply-To: webmaster@forma-test' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
-
-mail($to, $subject, $message, $headers);
